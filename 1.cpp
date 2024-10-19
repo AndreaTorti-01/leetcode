3c0a4c90b0ddec378de849b7473e76c6c09ba498
AndreaTorti-01 <andreatorti@proton.me>
2024-10-19 22:48:12 +0200

    init

---
+++ 1.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <execution>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // order nums 
+        vector<int> sorted_nums = nums;
+        sort(execution::par, sorted_nums.begin(), sorted_nums.end());
+
+        // find the two numbers
+        int i = 0, j = sorted_nums.size() - 1, sum;
+        while (i < j) {
+            sum = sorted_nums[i] + sorted_nums[j];
+            if (sum == target) {
+                break;
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        // find the indices of the two numbers in the original vector and return their indices
+        vector<int> res;
+        for (int k = 0; k < nums.size(); k++) {
+            if (nums[k] == sorted_nums[i] || nums[k] == sorted_nums[j]) {
+                res.push_back(k);
+            }
+        }
+
+        return res;
+    }
+};
---
+++ 2.cpp
@@ -0,0 +1,71 @@
+#include <thread>
+#include <vector>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr)
+    {
+    }
+    ListNode(int x) : val(x), next(nullptr)
+    {
+    }
+    ListNode(int x, ListNode *next) : val(x), next(next)
+    {
+    }
+};
+
+class Solution
+{
+  public:
+    // Function to add two numbers represented by linked lists
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        // Initialize current node to dummy head of the returning list.
+        ListNode *dummyHead = new ListNode(0);
+        // Initialize p and q to head of l1 and l2 respectively.
+        ListNode *p = l1, *q = l2, *curr = dummyHead;
+        // Initialize carry to 0.
+        int carry = 0;
+
+        // Loop through lists l1 and l2 until you reach both ends.
+        while (p != nullptr || q != nullptr)
+        {
+            // Set x to node p's value. If p has reached the end of l1, set to 0.
+            int x = (p != nullptr) ? p->val : 0;
+            // Set y to node q's value. If q has reached the end of l2, set to 0.
+            int y = (q != nullptr) ? q->val : 0;
+            // Set sum = x + y + carry
+            int sum = carry + x + y;
+            // Update carry = sum / 10
+            carry = sum / 10;
+            // Create a new node with the digit value of (sum mod 10) and set it to current node's next, then advance current node to next.
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            // Advance both p and q.
+            if (p != nullptr)
+                p = p->next;
+            if (q != nullptr)
+                q = q->next;
+        }
+        // Check if carry = 1, if so append a new node with digit 1 to the returning list.
+        if (carry > 0)
+        {
+            curr->next = new ListNode(carry);
+        }
+        // Return dummy head's next node.
+        return dummyHead->next;
+    }
+};
